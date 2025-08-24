0d2c80ad58636b2324bf1fc80c90e4badb2b3c57
Pom4ek <pom4ekoffical@mail.ru>
2025-08-24 22:32:45 +0300

    init

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    # You can fill .env file or write directly in here
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    
+    PROJECT_NAME: str = "LztMarketPriceScript"
+    
+    MARKET_API_URL: str = "https://prod-api.lzt.market/"
+    MARKET_API_TOKEN: str = ""
+    
+    PERCENT_OF_SELLER: int = 20
+
+
+
+settings = Settings()
